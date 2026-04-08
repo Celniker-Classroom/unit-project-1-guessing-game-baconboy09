a077cade901c5da55bb4f788a070f1dca24991f1
baconboy09 <123681424+baconboy09@users.noreply.github.com>
2026-04-08 23:18:12 +0000

    someone in your life

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 // add javascript here
+function gid(id){
+    return document.getElementById(id)
+}
+function gva(name){
+    return document.querySelector(`input[name="${name}"]:checked`).value;
+}
+function setMsg(text) {
+    gid('msg').innerHTML = text;
+}
+msgtext="";
+correct=0
+gid("playBtn").addEventListener("click", function(){
+        num = Math.floor(Math.random()*parseInt(gva('level')))
+        setMsg('guess the number!')
+        gid('guessBtn').disabled = false
+        gid('giveUpBtn').disabled = false
+        gid('playBtn').disabled = true
+        correct = 0
+    }
+)
+
+gid('guessBtn').addEventListener('click', function(){
+        guess = parseInt(gid('guess').value)
+         
+            if (!(isNaN(guess))){
+                if (guess > num){
+                    msgtext='guess lower'
+                } else if (guess < num ) {
+                    msgtext='guess higher'
+                } else {
+                    msgtext='Correct!'
+                    correct = 1
+                    gid('guessBtn').disabled = true
+                    gid('playBtn').disabled = false
+                }
+                if(!correct==1){
+                    warmth = Math.abs(guess-num)
+                    if (warmth <= 2){
+                        msgtext+=', hot!'
+                    } else if (warmth <= 5){
+                        msgtext+=', warm!'
+                    } else {
+                        msgtext+=', cold!'
+                    }
+                }
+            setMsg(msgtext)
+        }})
+
+gid('giveUpBtn').addEventListener('click', function(){
+    setMsg('The number was ' + num + '.');
+    gid('guessBtn').disabled = true;
+    gid('giveUpBtn').disabled = true;
+    gid('playBtn').disabled = false;
+    gid('guess').value = "";
+});
